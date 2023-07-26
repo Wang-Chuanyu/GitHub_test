4cadb5e22dccdff38d8e76dc31dfee8ab7fc6f08
HyacinthDR <c_y_wangfit@163.com>
2023-07-26 16:12:49 +0800

    jhkhkhkl

---
+++ src/main/java/com/hyacinth/GitTest.java
@@ -0,0 +1,12 @@
+package com.hyacinth;
+
+/**
+ * @author HyacinthLin
+ * @create 2023-07-26-16:03
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
