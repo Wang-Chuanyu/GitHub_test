1e374b0a05cbfbdf73bf7eb23659d202b281516a
HyacinthDR <c_y_wangfit@163.com>
2023-07-26 16:27:39 +0800

    master_commit

--- src/main/java/com/hyacinth/GitTest.java
+++ src/main/java/com/hyacinth/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello world");
         System.out.println("hello world222");
         System.out.println("hello world333");
+        System.out.println("hello worldmaster");
     }
 }
